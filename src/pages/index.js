b45f4e55b2a4b139e9baaaa005eb6d1928b9846d
xulanshan <xulanshan@wahaha.com.cn>
2021-11-29 15:26:30 +0800

    添加配置

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,7 +1,7 @@
 /*
  * @Author: your name
  * @Date: 2021-11-29 13:33:56
- * @LastEditTime: 2021-11-29 14:41:49
+ * @LastEditTime: 2021-11-29 15:23:30
  * @LastEditors: Please set LastEditors
  * @Description: 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
  * @FilePath: /my-website/src/pages/index.js
@@ -17,8 +17,9 @@ import HomepageFeatures from '../components/HomepageFeatures';
 function HomepageHeader() {
   const { siteConfig } = useDocusaurusContext();
   return (
-    <header className={clsx(styles.mainBg)}>
-      <div className="container">
+    <header className={styles.header}>
+      <div className={styles.container}>
+        <h1>SIMPLE</h1>
       </div>
     </header>
   );
