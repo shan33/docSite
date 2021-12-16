80d38d7cd98b325008427f3fb2987061088b21c3
xulanshan <xulanshan@wahaha.com.cn>
2021-12-16 11:30:21 +0800

    修改首页文字

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,7 +1,7 @@
 /*
  * @Author: your name
  * @Date: 2021-11-29 13:33:56
- * @LastEditTime: 2021-11-29 15:23:30
+ * @LastEditTime: 2021-12-16 11:29:36
  * @LastEditors: Please set LastEditors
  * @Description: 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
  * @FilePath: /my-website/src/pages/index.js
@@ -12,14 +12,15 @@ import Layout from '@theme/Layout';
 import Link from '@docusaurus/Link';
 import useDocusaurusContext from '@docusaurus/useDocusaurusContext';
 import styles from './index.module.css';
-import HomepageFeatures from '../components/HomepageFeatures';
 
 function HomepageHeader() {
-  const { siteConfig } = useDocusaurusContext();
+  // const { siteConfig } = useDocusaurusContext();
   return (
     <header className={styles.header}>
       <div className={styles.container}>
-        <h1>SIMPLE</h1>
+        <h2>简单点</h2>
+        <h1>不积跬步无以至千里</h1>
+        <h2>百般算计不如一颗单纯的心</h2>
       </div>
     </header>
   );
@@ -32,8 +33,6 @@ export default function Home() {
       title={`Hello from ${siteConfig.title}`}
       description="Description will go into a meta tag in <head />">
       <HomepageHeader />
-      <main>
-      </main>
     </Layout>
   );
 }
